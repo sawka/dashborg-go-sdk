1ae7cf3757ca3ddb3f7a6ea9b568be300ff8ddad
sawka <mike.sawka@gmail.com>
2021-01-24 23:54:11 -0800

    use .model instead of .state for Modal data

--- cmd/demo2/demo2_main.go
+++ cmd/demo2/demo2_main.go
@@ -206,13 +206,13 @@ func main() {
 		return nil
 	})
 	dash.RegisterPanelHandler("demo2", "/acc/refresh-accounts", func(req *dash.PanelRequest) error {
-		req.SetData("state.selaccid", nil)
+		req.SetData("model.selaccid", nil)
 		req.InvalidateData("/accounts/.*")
 		return nil
 	})
 	dash.RegisterPanelHandler("demo2", "/acc/regen-acclist", func(req *dash.PanelRequest) error {
 		Model.RegenAccounts()
-		req.SetData("state.selaccid", nil)
+		req.SetData("model.selaccid", nil)
 		req.InvalidateData("/accounts/.*")
 		return nil
 	})
@@ -223,15 +223,15 @@ func main() {
 		}
 		Model.RemoveAcc(accId)
 		req.InvalidateData("/accounts/.*")
-		req.SetData("state.selaccid", nil)
+		req.SetData("model.selaccid", nil)
 		return nil
 	})
 	dash.RegisterPanelHandler("demo2", "/acc/open-create-account-modal", func(req *dash.PanelRequest) error {
-		req.SetData("state.createAccountModal.open", true)
+		req.SetData("model.createAccountModal.open", true)
 		return nil
 	})
 	dash.RegisterPanelHandler("demo2", "/acc/close-modal", func(req *dash.PanelRequest) error {
-		req.SetData("state.createAccountModal.open", false)
+		req.SetData("model.createAccountModal.open", false)
 		return nil
 	})
 	dash.RegisterPanelHandler("demo2", "/acc/create-account", func(req *dash.PanelRequest) error {
@@ -242,13 +242,13 @@ func main() {
 		}
 		errors := panelModel.CreateData.Validate()
 		if len(errors) > 0 {
-			req.SetData("state.create.errors", errors)
+			req.SetData("model.create.errors", errors)
 			return nil
 		}
-		req.SetData("state.create.errors", nil)
+		req.SetData("model.create.errors", nil)
 		newAccId := Model.CreateAcc(panelModel.CreateData.Name, panelModel.CreateData.Email)
-		req.SetData("state.createAccountModal.open", false)
-		req.SetData("state.selaccid", newAccId)
+		req.SetData("model.createAccountModal.open", false)
+		req.SetData("model.selaccid", newAccId)
 		req.InvalidateData("/accounts/.*")
 		return nil
 	})
