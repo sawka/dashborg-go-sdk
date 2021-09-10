a2d489e7266a7751b4f95637aa6821074cfa2d17
sawka <mike.sawka@gmail.com>
2021-09-09 17:26:10 -0700

    Update AuthAtom to contain RoleList instead of just Role to better reflect server state

--- pkg/dash/auth.go
+++ pkg/dash/auth.go
@@ -8,18 +8,34 @@ const MaxAuthExp = 24 * time.Hour
 const AuthScopeZone = "zone"
 
 type AuthAtom struct {
-	Scope string                 `json:"scope"` // scope of this atom acc, zone, app
-	Type  string                 `json:"type"`  // auth type (password, noauth, dashborg, deauth, or user-defined)
-	Ts    int64                  `json:"ts"`    // expiration Ts (ms) of this auth atom
-	Role  string                 `json:"role"`
-	Id    string                 `json:"id,omitempty"`
-	Data  map[string]interface{} `json:"data,omitempty"`
+	Scope    string                 `json:"scope"` // scope of this atom acc, zone, app
+	Type     string                 `json:"type"`  // auth type (password, noauth, dashborg, deauth, or user-defined)
+	Ts       int64                  `json:"ts"`    // expiration Ts (ms) of this auth atom
+	RoleList []string               `json:"role"`
+	Id       string                 `json:"id,omitempty"`
+	Data     map[string]interface{} `json:"data,omitempty"`
 }
 
 // Returns AuthAtom role.  If AuthAtom is nil, returns "public"
-func (aa *AuthAtom) GetRole() string {
+func (aa *AuthAtom) HasRole(role string) bool {
 	if aa == nil {
-		return ""
+		return false
 	}
-	return aa.Role
+	for _, checkRole := range aa.RoleList {
+		if checkRole == role {
+			return true
+		}
+	}
+	return false
+}
+
+func (aa *AuthAtom) IsSuper() bool {
+	return aa.HasRole(RoleSuper)
+}
+
+func (aa *AuthAtom) GetRoleList() []string {
+	if aa == nil {
+		return nil
+	}
+	return aa.RoleList
 }
